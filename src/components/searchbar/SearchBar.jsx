17f0068341c23a0ace9c04d643cf7c482258ce67
ilarne <larner.isabel@gmail.com>
2018-09-16 17:03:23 +0100

    removing old file locations

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -1,16 +1,52 @@
 import React, { Component } from "react";
-import "../App.css";
+import "../../App.css";
+import ResultsContainer from "../resultscontainer/ResultsContainer";
+import axios from "axios";
+
+const BASE_API_URL = "http://localhost:3001/data";
 
 class SearchBar extends Component {
+  state = { data: [], query: "" };
+
+  async filterManagers() {
+    const response = await axios.get(
+      `${BASE_API_URL}?attributes.name_like=${this.state.query}`,
+      null,
+      {
+        data: {},
+        headers: { "Content-Type": "application/vnd.api+json" }
+      }
+    );
+    this.setState({ data: response.data });
+  }
+
+  handleUserInput = () => {
+    this.setState({
+      query: this.input.value
+    });
+    if (this.state.query.length === 1) {
+      this.setState({
+        query: this.input.value,
+        data: []
+      });
+    } else {
+      this.filterManagers(this.state.query);
+    }
+  };
+
   render() {
     return (
-      <div>
-        <input
-          className="text text-primary"
-          type="text"
-          placeholder="Choose Manager"
-        />
-      </div>
+      <React.Fragment>
+        <form>
+          <input
+            className="text text-primary searchBar"
+            ref={input => (this.input = input)}
+            placeholder="Choose Manager"
+            onChange={this.handleUserInput}
+          />
+        </form>
+        <ResultsContainer managers={this.state.data} />
+      </React.Fragment>
     );
   }
 }
