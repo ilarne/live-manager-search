ae6c52c723c739f89ed1b1437e1490a6a6846b12
ilarne <larner.isabel@gmail.com>
2018-09-18 12:03:33 +0100

    added API call to retrieve email using ID from employee relationship field. N.B. while this 'appears' to work from a user perspective it is known that this is only a work around and lacks the specified search functionality for email

--- src/components/resultscontainer/ResultsContainer.jsx
+++ src/components/resultscontainer/ResultsContainer.jsx
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import Result from "../result/Result";
 import "./ResultsContainer.css";
+import axios from "axios";
 
-function ResultsContainer(props) {
-  const { managers } = props;
+const BASE_API_URL = "http://localhost:3001/";
 
-  return (
-    <React.Fragment>
-      <ul className="results-container">
-        {managers.map(manager => (
-          <Result
-            key={manager.id}
-            fullName={manager.attributes.name}
-            firstName={manager.attributes.firstName}
-            lastName={manager.attributes.lastName}
-          />
-        ))}
-      </ul>
-    </React.Fragment>
-  );
+class ResultsContainer extends Component {
+  // state = { email: "" };
+
+  async getEmail(manager) {
+    const emailId = manager.relationships.account.data.id;
+    if (emailId) {
+      const response = await axios.get(
+        `${BASE_API_URL}included/${emailId}`,
+        null,
+        {
+          data: {},
+          headers: { Accept: "application/vnd.api+json" }
+        }
+      );
+      const email = response.data.attributes.email;
+      return email;
+    }
+  }
+
+  render() {
+    const { managers } = this.props;
+    return (
+      <React.Fragment>
+        <ul className="results-container">
+          {managers.map(manager => (
+            <Result
+              key={manager.id}
+              fullName={manager.attributes.name}
+              firstName={manager.attributes.firstName}
+              lastName={manager.attributes.lastName}
+              email={this.getEmail(manager)}
+            />
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
 }
 
 export default ResultsContainer;
