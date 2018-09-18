167953cd6219abf55b9001f6cd021ffdbd65d1f8
ilarne <larner.isabel@gmail.com>
2018-09-18 10:14:14 +0100

    changing ResultsContainer into a functional component

--- src/components/resultscontainer/ResultsContainer.jsx
+++ src/components/resultscontainer/ResultsContainer.jsx
@@ -1,26 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import Result from "../result/Result";
 import "./ResultsContainer.css";
 
-class ResultsContainer extends Component {
-  render() {
-    const { managers } = this.props;
+function ResultsContainer(props) {
+  const { managers } = props;
 
-    return (
-      <React.Fragment>
-        <ul className="results-container">
-          {managers.map(manager => (
-            <Result
-              key={manager.id}
-              fullName={manager.attributes.name}
-              firstName={manager.attributes.firstName}
-              lastName={manager.attributes.lastName}
-            />
-          ))}
-        </ul>
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <ul className="results-container">
+        {managers.map(manager => (
+          <Result
+            key={manager.id}
+            fullName={manager.attributes.name}
+            firstName={manager.attributes.firstName}
+            lastName={manager.attributes.lastName}
+          />
+        ))}
+      </ul>
+    </React.Fragment>
+  );
 }
 
 export default ResultsContainer;
